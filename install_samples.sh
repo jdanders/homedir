070a74d56b7017daf3c414a1dad281239845f126
Jordan Anderson <jordan@example.com>
2015-12-12 20:17:33 -0700

    Add install script

---
+++ install_samples.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os,sys
+
+files = [ii for ii in os.listdir('.') if "sample_" in ii]
+for file in files:
+    homedir = os.path.expanduser("~")
+    newfile = homedir+os.sep+file.replace('sample_','')
+    print "Copying %s to %s"%(file,newfile)
+    if os.path.exists(newfile):
+        print "Backing up existing copy"
+        result=os.popen('cp %s %s.homedirbackup'%(newfile,newfile)).read()
+    result=os.popen('cp %s %s'%(file,newfile))
