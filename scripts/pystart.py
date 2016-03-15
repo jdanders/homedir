4811de79c618134cec922e401ec447ef156ffc78
Jordan Anderson <janderson@secturion.com>
2016-03-15 12:08:38 -0600

    Add pprint to default python includes

--- scripts/pystart.py
+++ scripts/pystart.py
@@ -1,5 +1,6 @@
 import os,sys,re
 from time import sleep
+from pprint import pprint
 home = os.path.expanduser('~')
 if (sys.version_info > (3, 0)):
   # Python 3 code in this block
@@ -8,4 +9,4 @@ else:
   # Python 2 code in this block
   execfile(home+'/homedir/scripts/hexecho.py')
 hexoff
-print ("Ran pystart and did import os,sys,re, sleep from time, and hexon/hexoff")
+print ("Ran pystart and did import os,sys,re,sleep,pprint, and hexon/hexoff")
