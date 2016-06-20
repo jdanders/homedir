d09ac7a3b525f4be2cbfcb0f8221227bce56a8fa
Jordan Anderson <janderson@secturion.com>
2016-06-20 13:43:51 -0600

    Add clog2 to pystart.py

--- scripts/pystart.py
+++ scripts/pystart.py
@@ -2,6 +2,9 @@ import os,sys,re
 from time import sleep
 from pprint import pprint
 home = os.path.expanduser('~')
+from math import log,ceil
+def clog2(num):
+  return int(ceil(log(num,2)))
 if (sys.version_info > (3, 0)):
   # Python 3 code in this block
   exec(open(home+'/homedir/scripts/hexecho.py').read())
@@ -9,4 +12,4 @@ else:
   # Python 2 code in this block
   execfile(home+'/homedir/scripts/hexecho.py')
 hexoff
-print ("Imported os,sys,re,sleep,pprint, and hexon/hexoff")
+print ("Imported os,sys,re,sleep,pprint. Defined clog2,hexon/hexoff")
