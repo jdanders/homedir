66ce6bde747a269df94e00a33effd633a8599345
Jordan Anderson <username@example.com>
2017-09-16 16:57:51 -0600

    make install script python3 compatible

--- install_samples.sh
+++ install_samples.sh
@@ -5,8 +5,8 @@ files = [ii for ii in os.listdir('.') if "sample_" in ii]
 for file in files:
     homedir = os.path.expanduser("~")
     newfile = homedir+os.sep+file.replace('sample_','')
-    print "Copying %s to %s"%(file,newfile)
+    print("Copying %s to %s"%(file,newfile))
     if os.path.exists(newfile):
-        print "Backing up existing copy"
+        print("Backing up existing copy")
         result=os.popen('cp %s %s.homedirbackup'%(newfile,newfile)).read()
     result=os.popen('cp %s %s'%(file,newfile))
