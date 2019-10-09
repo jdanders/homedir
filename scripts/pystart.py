43773f2ac8f3da0cd210baa4a49e4246df630869
Jordan Anderson <janderson@secturion.com>
2019-10-09 12:09:17 -0600

    Update external sources, add git alias 'amend', fix py clog for 0 input

--- scripts/pystart.py
+++ scripts/pystart.py
@@ -6,6 +6,8 @@ from math import log,ceil
 sys.ps1 = '\001\033[96m\002>>> \001\033[0m\002'
 sys.ps2 = '\001\033[96m\002... \001\033[0m\002'
 def clog2(num):
+  if num == 0:
+    return 0
   return int(ceil(log(num,2)))
 if (sys.version_info > (3, 0)):
   # Python 3 code in this block
