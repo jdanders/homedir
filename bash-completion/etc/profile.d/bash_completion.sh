6613f5359bb82a143c2d36e08521880e2574eb9c
Jordan Anderson <username@example.com>
2024-03-28 15:34:17 -0600

    Update bash-completion to 2.12.0

--- bash-completion/etc/profile.d/bash_completion.sh
+++ bash-completion/etc/profile.d/bash_completion.sh
@@ -1,4 +1,4 @@
-# shellcheck shell=sh disable=SC1091,SC2039,SC2166
+# shellcheck shell=sh disable=SC1091,SC2166,SC2268,SC3028,SC3044,SC3054
 # Check for interactive bash and that we haven't already been sourced.
 if [ "x${BASH_VERSION-}" != x -a "x${PS1-}" != x -a "x${BASH_COMPLETION_VERSINFO-}" = x ]; then
 
@@ -7,9 +7,9 @@ if [ "x${BASH_VERSION-}" != x -a "x${PS1-}" != x -a "x${BASH_COMPLETION_VERSINFO
         [ "${BASH_VERSINFO[0]}" -eq 4 -a "${BASH_VERSINFO[1]}" -ge 2 ]; then
         [ -r "${XDG_CONFIG_HOME:-$HOME/.config}/bash_completion" ] &&
             . "${XDG_CONFIG_HOME:-$HOME/.config}/bash_completion"
-        if shopt -q progcomp && [ -r /home/janderson/homedir/testbashcomplete/share/bash-completion/bash_completion ]; then
+        if shopt -q progcomp && [ -r /tmp/testbash/share/bash-completion/bash_completion ]; then
             # Source completion code.
-            . /home/janderson/homedir/testbashcomplete/share/bash-completion/bash_completion
+            . /tmp/testbash/share/bash-completion/bash_completion
         fi
     fi
 
